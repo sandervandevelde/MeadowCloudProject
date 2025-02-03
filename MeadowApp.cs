74c8d0f161d0723c69fd68ac9a4bb92be7d78876
sande <sande@LAPTOP-COLAAI4C>
2025-02-03 20:01:09 +0100

    Add project files.

---
+++ MeadowApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+using Meadow.Logging;
+
+namespace MeadowCloudProject
+{
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+
+            var cloudLogger = new CloudLogger();
+
+            Resolver.Log.AddProvider(cloudLogger);
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            Resolver.Log.Info("Hello, Meadow Core-Compute!");
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
